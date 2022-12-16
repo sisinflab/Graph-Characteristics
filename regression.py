34991ff4d4480be9b01f3339fe16cf3f0e44eafb
danielemalitesta <d.malitesta@gmail.com>
2022-12-16 12:52:45 +0100

    Regression made.

--- regression.py
+++ regression.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import numpy as np
 import argparse
-from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as sm
+
+np.random.seed(1234)
 
 parser = argparse.ArgumentParser(description="Run regression.")
 parser.add_argument('--dataset', type=str, default='gowalla')
@@ -13,27 +16,36 @@ args = parser.parse_args()
 results = pd.read_csv(f'data/{args.dataset}/characteristics_{args.metric}_{args.start_id}_{args.end_id}.tsv', sep='\t')
 models = ['LightGCN', 'DGCF', 'UltraGCN', 'SVDGCN']
 characteristics = args.characteristics.split(' ')
-characteristics_w = ['intercept'] + [c + '_w' for c in characteristics]
+results[characteristics] = results[characteristics].apply(
+        lambda x: (x - x.mean()))
+msk = np.random.rand(len(results)) < 0.8
+test = results[~msk]
+train = results[msk]
 
 models_results = []
 
 for idx, m in enumerate(models):
-    X = results[characteristics].to_numpy()
-    y = results[m].to_numpy()
-    reg = LinearRegression().fit(X, y)
-    score = reg.score(X, y)
-    adjusted_score = 1 - (1 - score) * ((X.shape[0] - 1) / (X.shape[0] - len(characteristics) - 1))
-    coefs = [reg.intercept_] + reg.coef_.tolist()
+    X = train[characteristics]
+    y = train[m]
+
+    formula_str_ml = y.name + ' ~ ' + '+'.join(characteristics)
+    model_ml = sm.ols(formula=formula_str_ml, data=train[characteristics+[m]])
+    fitted_ml = model_ml.fit(cov_type='HC1')
+    # predicts_ml = fitted_ml.predict(test)
+
+    # mean_absolute_error_ml = mean_absolute_error(test[characteristics],
+    #                                                  predicts_ml)
+    # root_mean_absolute_error_ml = math.sqrt(
+    #     mean_absolute_error(test[characteristics], predicts_ml))
+    # rmse_ml = mean_squared_error(test[characteristics], predicts_ml)
+
     models_results.append({
         'model': m,
-        'score': score,
-        'adjusted_score': adjusted_score
+        'score': fitted_ml.rsquared,
+        'adjusted_score': fitted_ml.rsquared_adj,
+        **fitted_ml.params.to_dict(),
+        **fitted_ml.pvalues.rename(lambda x: 'p_'+x).to_dict()
     })
-
-    for idxx, c in enumerate(characteristics):
-        models_results[idx][c] = X.reshape(-1)[idxx]
-    for idxx, c in enumerate(characteristics_w):
-        models_results[idx][c] = coefs[idxx]
 
 df = pd.DataFrame.from_dict(models_results)
 df.to_csv(f'data/{args.dataset}/regression-{args.metric}-{args.start_id}-{args.end_id}.tsv', sep='\t', index=None)
