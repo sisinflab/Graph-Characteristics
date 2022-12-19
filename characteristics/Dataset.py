208569b8c790276401aea950b8b428e63b517351
dmalitesta <d.malitesta@gmail.com>
2022-12-19 11:03:46 +0100

    Added graph characteristics.

--- characteristics/Dataset.py
+++ characteristics/Dataset.py
@@ -73,7 +73,22 @@ class RecommendationDataset:
         self._gini_item = None
         self._gini_user = None
         self.metrics = ['transactions', 'space_size', 'space_size_log', 'shape', 'shape_log', 'density', 'density_log',
-                        'gini_item', 'gini_user']
+                        'gini_item', 'gini_user',
+                        'average_degree', 'average_degree_users', 'average_degree_items',
+                        'average_degree_log', 'average_degree_users_log', 'average_degree_items_log',
+                        'average_clustering_coefficient_dot',
+                        'average_clustering_coefficient_min',
+                        'average_clustering_coefficient_max',
+                        'average_clustering_coefficient_dot_log',
+                        'average_clustering_coefficient_min_log',
+                        'average_clustering_coefficient_max_log',
+                        'average_clustering_coefficient_dot_users', 'average_clustering_coefficient_dot_items',
+                        'average_clustering_coefficient_min_users', 'average_clustering_coefficient_min_items',
+                        'average_clustering_coefficient_max_users', 'average_clustering_coefficient_max_items',
+                        'average_clustering_coefficient_dot_users_log', 'average_clustering_coefficient_dot_items_log',
+                        'average_clustering_coefficient_min_users_log', 'average_clustering_coefficient_min_items_log',
+                        'average_clustering_coefficient_max_users_log', 'average_clustering_coefficient_max_items_log',
+                        'degree_assortativity_users', 'degree_assortativity_items']
         self._ratings_per_user = None
         self._ratings_per_item = None
 
@@ -149,43 +164,38 @@ class RecommendationDataset:
 
     def get_metric(self, metric):
         assert metric in self.metrics
-        return self.__getattribute__(metric)
+        func = getattr(self, metric)
+        return func()
 
-    @property
     def space_size(self):
         if self._space_size is None:
             scale_factor = 1000
             self._space_size = math.sqrt(self._n_users * self._n_items) / scale_factor
         return self._space_size
 
-    @property
     def space_size_log(self):
         if self._space_size_log is None:
-            self._space_size_log = math.log10(self.space_size)
+            self._space_size_log = math.log10(self.space_size())
         return self._space_size_log
 
-    @property
     def shape(self):
         if self._shape is None:
             self._shape = self._n_users / self._n_items
         return self._shape
 
-    @property
     def shape_log(self):
         if self._shape_log is None:
-            self._shape_log = math.log10(self.shape)
+            self._shape_log = math.log10(self.shape())
         return self._shape_log
 
-    @property
     def density(self):
         if self._density is None:
             self._density = self.transactions / (self._n_users * self._n_items)
         return self._density
 
-    @property
     def density_log(self):
         if self._density_log is None:
-            self._density_log = math.log10(self.density)
+            self._density_log = math.log10(self.density())
         return self._density_log
 
     @staticmethod
@@ -195,26 +205,22 @@ class RecommendationDataset:
             total += np.sum(np.abs(xi - x[i:]))
         return total / (len(x) ** 2 * np.mean(x))
 
-    @property
     def gini_item(self):
         if self._gini_item is None:
             self._gini_item = self.gini(np.array(list(self.sorted_items.values())))
         return self._gini_item
 
-    @property
     def gini_user(self):
         if self._gini_user is None:
             self._gini_user = self.gini(np.array(list(self.sorted_users.values())))
         return self._gini_user
 
-    @property
     def ratings_per_user(self):
 
         if self._ratings_per_user is None:
             self._ratings_per_user = self.transactions / self.n_users
         return self._ratings_per_user
 
-    @property
     def ratings_per_item(self):
 
         if self._ratings_per_item is None:
@@ -229,13 +235,249 @@ class GraphDataset(RecommendationDataset):
 
         super(GraphDataset, self).__init__(data)
 
+        self.bipartite_graph = self.networkx_bipartite_graph()
+        self.num_edges = len(self.bipartite_graph.edges)
+        self.user_nodes, self.item_nodes = bipartite.sets(self.bipartite_graph)
+        self.num_user_nodes, self.num_item_nodes = len(self.user_nodes), len(self.item_nodes)
+        self.user_graph, self.item_graph = None, None
+
+        # metrics
+
+        # degree
+        self._average_degree = None
+        self._average_degree_users = None
+        self._average_degree_items = None
+        self._average_degree_log = None
+        self._average_degree_users_log = None
+        self._average_degree_items_log = None
+
+        # clustering
+        self._average_clustering_coefficient_dot = None
+        self._average_clustering_coefficient_min = None
+        self._average_clustering_coefficient_max = None
+        self._average_clustering_coefficient_dot_users = None
+        self._average_clustering_coefficient_dot_items = None
+        self._average_clustering_coefficient_min_users = None
+        self._average_clustering_coefficient_min_items = None
+        self._average_clustering_coefficient_max_users = None
+        self._average_clustering_coefficient_max_items = None
+        self._average_clustering_coefficient_dot_log = None
+        self._average_clustering_coefficient_min_log = None
+        self._average_clustering_coefficient_max_log = None
+        self._average_clustering_coefficient_dot_users_log = None
+        self._average_clustering_coefficient_dot_items_log = None
+        self._average_clustering_coefficient_min_users_log = None
+        self._average_clustering_coefficient_min_items_log = None
+        self._average_clustering_coefficient_max_users_log = None
+        self._average_clustering_coefficient_max_items_log = None
+
+        # assortativity
+        self._degree_assortativity = None
+        self._degree_assortativity_users = None
+        self._degree_assortativity_items = None
+
+    def average_degree(self):
+
+        if self._average_degree is None:
+            self._average_degree = (2 * self.num_edges) / (self.num_user_nodes + self.num_item_nodes)
+
+        return self._average_degree
+
+    def average_degree_log(self):
+
+        if self._average_degree_log is None:
+            self._average_degree_log = math.log10(self.average_degree())
+
+        return self._average_degree_log
+
+    def average_degree_users(self):
+
+        if self._average_degree_users is None:
+            self._average_degree_users = self.num_edges / self.num_user_nodes
+
+        return self._average_degree_users
+
+    def average_degree_users_log(self):
+
+        if self._average_degree_users_log is None:
+            self._average_degree_users_log = math.log10(self.average_degree_users())
+
+        return self._average_degree_users_log
+
+    def average_degree_items(self):
+
+        if self._average_degree_items is None:
+            self._average_degree_items = self.num_edges / self.num_item_nodes
+
+        return self._average_degree_items
+
+    def average_degree_items_log(self):
+
+        if self._average_degree_items_log is None:
+            self._average_degree_items_log = math.log10(self.average_degree_items())
+
+        return self._average_degree_items_log
+
+    def average_clustering_coefficient_dot(self):
+
+        if self._average_clustering_coefficient_dot is None:
+            self._average_clustering_coefficient_dot = bipartite.average_clustering(self.bipartite_graph,
+                                                                                    mode='dot')
+        return self._average_clustering_coefficient_dot
+
+    def average_clustering_coefficient_dot_log(self):
+
+        if self._average_clustering_coefficient_dot_log is None:
+            self._average_clustering_coefficient_dot_log = math.log10(self.average_clustering_coefficient_dot())
+        return self._average_clustering_coefficient_dot_log
+
+    def average_clustering_coefficient_min(self):
+
+        if self._average_clustering_coefficient_min is None:
+            self._average_clustering_coefficient_min = bipartite.average_clustering(self.bipartite_graph,
+                                                                                    mode='min')
+        return self._average_clustering_coefficient_min
+
+    def average_clustering_coefficient_min_log(self):
+
+        if self._average_clustering_coefficient_min_log is None:
+            self._average_clustering_coefficient_min_log = math.log10(self.average_clustering_coefficient_min())
+        return self._average_clustering_coefficient_min_log
+
+    def average_clustering_coefficient_max(self):
+
+        if self._average_clustering_coefficient_max is None:
+            self._average_clustering_coefficient_max = bipartite.average_clustering(self.bipartite_graph,
+                                                                                    mode='max')
+        return self._average_clustering_coefficient_max
+
+    def average_clustering_coefficient_max_log(self):
+
+        if self._average_clustering_coefficient_max_log is None:
+            self._average_clustering_coefficient_max_log = math.log10(self.average_clustering_coefficient_max())
+        return self._average_clustering_coefficient_max_log
+
+    def average_clustering_coefficient_dot_users(self):
+
+        if self._average_clustering_coefficient_dot_users is None:
+            self._average_clustering_coefficient_dot_users = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='dot',
+                                                                                          nodes=self.user_nodes)
+        return self._average_clustering_coefficient_dot_users
+
+    def average_clustering_coefficient_dot_users_log(self):
+
+        if self._average_clustering_coefficient_dot_users_log is None:
+            self._average_clustering_coefficient_dot_users_log = math.log10(
+                self.average_clustering_coefficient_dot_users()
+            )
+        return self._average_clustering_coefficient_dot_users_log
+
+    def average_clustering_coefficient_dot_items(self):
+
+        if self._average_clustering_coefficient_dot_items is None:
+            self._average_clustering_coefficient_dot_items = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='dot',
+                                                                                          nodes=self.item_nodes)
+        return self._average_clustering_coefficient_dot_items
+
+    def average_clustering_coefficient_dot_items_log(self):
+
+        if self._average_clustering_coefficient_dot_items_log is None:
+            self._average_clustering_coefficient_dot_items_log = math.log10(
+                self.average_clustering_coefficient_dot_items()
+            )
+        return self._average_clustering_coefficient_dot_items_log
+
+    def average_clustering_coefficient_min_users(self):
+
+        if self._average_clustering_coefficient_min_users is None:
+            self._average_clustering_coefficient_min_users = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='min',
+                                                                                          nodes=self.user_nodes)
+        return self._average_clustering_coefficient_min_users
+
+    def average_clustering_coefficient_min_users_log(self):
+
+        if self._average_clustering_coefficient_min_users_log is None:
+            self._average_clustering_coefficient_min_users_log = math.log10(
+                self.average_clustering_coefficient_min_users()
+            )
+        return self._average_clustering_coefficient_min_users_log
+
+    def average_clustering_coefficient_min_items(self):
+
+        if self._average_clustering_coefficient_min_items is None:
+            self._average_clustering_coefficient_min_items = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='min',
+                                                                                          nodes=self.item_nodes)
+        return self._average_clustering_coefficient_min_items
+
+    def average_clustering_coefficient_min_items_log(self):
+
+        if self._average_clustering_coefficient_min_items_log is None:
+            self._average_clustering_coefficient_min_items_log = math.log10(
+                self.average_clustering_coefficient_min_items()
+            )
+        return self._average_clustering_coefficient_min_items_log
+
+    def average_clustering_coefficient_max_users(self):
+
+        if self._average_clustering_coefficient_max_users is None:
+            self._average_clustering_coefficient_max_users = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='max',
+                                                                                          nodes=self.user_nodes)
+        return self._average_clustering_coefficient_max_users
+
+    def average_clustering_coefficient_max_users_log(self):
+
+        if self._average_clustering_coefficient_max_users_log is None:
+            self._average_clustering_coefficient_max_users_log = math.log10(
+                self.average_clustering_coefficient_max_users()
+            )
+        return self._average_clustering_coefficient_max_users_log
+
+    def average_clustering_coefficient_max_items(self):
+
+        if self._average_clustering_coefficient_max_items is None:
+            self._average_clustering_coefficient_max_items = bipartite.average_clustering(self.bipartite_graph,
+                                                                                          mode='max',
+                                                                                          nodes=self.item_nodes)
+        return self._average_clustering_coefficient_max_items
+
+    def average_clustering_coefficient_max_items_log(self):
+
+        if self._average_clustering_coefficient_max_items_log is None:
+            self._average_clustering_coefficient_max_items_log = math.log10(
+                self.average_clustering_coefficient_max_items()
+            )
+        return self._average_clustering_coefficient_max_items_log
+
+    def degree_assortativity_users(self):
+
+        if self._degree_assortativity_users is None and self.user_graph is None:
+            self.user_graph = bipartite.projected_graph(self.bipartite_graph, self.user_nodes)
+            self._degree_assortativity_users = networkx.degree_pearson_correlation_coefficient(self.user_graph,
+                                                                                               nodes=self.user_nodes)
+
+        return self._degree_assortativity_users
+
+    def degree_assortativity_items(self):
+
+        if self._degree_assortativity_items is None and self.item_graph is None:
+            self.item_graph = bipartite.projected_graph(self.bipartite_graph, self.item_nodes)
+            self._degree_assortativity_items = networkx.degree_pearson_correlation_coefficient(self.item_graph,
+                                                                                               nodes=self.item_nodes)
+
+        return self._degree_assortativity_items
+
     def networkx_bipartite_graph(self):
         # build undirected and bipartite graph with networkx
         print(f'{self.__class__.__name__}: building a bipartite graph with networkx')
         graph = networkx.Graph()
         graph.add_nodes_from(self._users, bipartite='users')
         graph.add_nodes_from(self._items, bipartite='items')
-        graph.add_edges_from(list(zip(self._dataset[0], self._dataset[1])))
+        graph.add_edges_from(list(zip(self._dataset[self.user_col], self._dataset[self.item_col])))
         return graph
 
     def connected_graph(self):
@@ -254,19 +496,20 @@ class GraphDataset(RecommendationDataset):
         return graph
 
     def make_connected(self):
-        graph = self.connected_graph()
+        self.bipartite_graph = self.connected_graph()
 
-        user_nodes, item_nodes = bipartite.sets(graph)
+        user_nodes, item_nodes = bipartite.sets(self.bipartite_graph)
 
         n_old_users = self._n_users
         n_old_items = self._n_items
 
         # filter out users and items
-        self._dataset = self._dataset[self._dataset[0].isin(user_nodes) & self._dataset[1].isin(item_nodes)]
-        assert user_nodes == set(self._dataset[0].unique()), f'{self.__class__.__name__}:' \
-                                                             f' a problem occurred during dataset filtering'
-        assert item_nodes == set(self._dataset[1].unique()), f'{self.__class__.__name__}:' \
-                                                             f' a problem occurred during dataset filtering'
+        self._dataset = self._dataset[
+            self._dataset[self.user_col].isin(user_nodes) & self._dataset[self.item_col].isin(item_nodes)]
+        assert user_nodes == set(self._dataset[self.user_col].unique()), f'{self.__class__.__name__}:' \
+                                                                         f' a problem occurred during dataset filtering'
+        assert item_nodes == set(self._dataset[self.item_col].unique()), f'{self.__class__.__name__}:' \
+                                                                         f' a problem occurred during dataset filtering'
 
         self._n_users = len(user_nodes)
         self._n_items = len(item_nodes)
--- generate_characteristics.py
+++ generate_characteristics.py
@@ -11,7 +11,37 @@ parser = argparse.ArgumentParser(description="Run generate characteristics.")
 parser.add_argument('--dataset', type=str, default='gowalla')
 parser.add_argument('--start_id', type=int, default=0)
 parser.add_argument('--end_id', type=int, default=99)
-parser.add_argument('--characteristics', type=str, default='space_size_log shape_log density_log gini_item gini_user')
+parser.add_argument('--characteristics', type=str,
+                    default='space_size_log '
+                            'shape_log '
+                            'density_log '
+                            'gini_item gini_user '
+                            'average_degree '
+                            'average_degree_users '
+                            'average_degree_items '
+                            'average_clustering_coefficient_dot '
+                            'average_clustering_coefficient_min '
+                            'average_clustering_coefficient_max '
+                            'average_clustering_coefficient_dot_users '
+                            'average_clustering_coefficient_dot_items '
+                            'average_clustering_coefficient_min_users '
+                            'average_clustering_coefficient_min_items '
+                            'average_clustering_coefficient_max_users '
+                            'average_clustering_coefficient_max_items '
+                            'average_degree_log '
+                            'average_degree_users_log '
+                            'average_degree_items_log '
+                            'average_clustering_coefficient_dot_log '
+                            'average_clustering_coefficient_min_log '
+                            'average_clustering_coefficient_max_log '
+                            'average_clustering_coefficient_dot_users_log '
+                            'average_clustering_coefficient_dot_items_log '
+                            'average_clustering_coefficient_min_users_log '
+                            'average_clustering_coefficient_min_items_log '
+                            'average_clustering_coefficient_max_users_log '
+                            'average_clustering_coefficient_max_items_log '
+                            'degree_assortativity_users '
+                            'degree_assortativity_items')
 parser.add_argument('--metric', type=str, default='Recall')
 parser.add_argument('--splitting', type=str, default='edge-dropout node-dropout')
 args = parser.parse_args()
